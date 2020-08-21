197783ef6394616a2efca69620673b25fdb78537
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-08-21 22:58:11 +0300

    created initial paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'movies'
+  },
+  {
+    path: 'movies',
+     component: MoviesComponent
+  },
+  {
+    path: 'movies/:id',
+    component: MovieDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
