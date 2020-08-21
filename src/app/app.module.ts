cf3df994c5a2956191acfd74dbd5fd9d7cc6d88c
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-08-21 15:34:11 +0300

    removed MovieService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
 import { MoviesComponent } from './movies/movies.component';
-import { MovieService } from './services/movie.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MovieComponent } from './movie/movie.component';
 import { FooterComponent } from './footer/footer.component';
@@ -25,7 +24,7 @@ import { FooterComponent } from './footer/footer.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [MovieService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
