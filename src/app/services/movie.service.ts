0b231ca9752db37b51fa35f6ad5b0239cd25033f
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-08-21 22:57:32 +0300

    added get movie by id request

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap, map } from 'rxjs/operators'
 import Movie from '../models/Movie';
+import MovieDetails from '../models/Movie-Details'
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie';
 const API_KEY = '?api_key=2802740079128ef16179086164af5f36';
@@ -12,31 +13,35 @@ const API_KEY = '?api_key=2802740079128ef16179086164af5f36';
 })
 
 export class MovieService {
+  
 
   constructor(private http: HttpClient) { }
 
   getPopularMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(`${BASE_URL}/popular${API_KEY}&page=1`)
       .pipe(
-        map(data=>data['results'].slice(0, 6))
+        map(data => data['results'].slice(0, 6))
       )
   }
   getInTheaterMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(`${BASE_URL}/now_playing${API_KEY}&page=1`)
-    .pipe(
-      map(data=>data['results'].slice(0, 6))
-    )
+      .pipe(
+        map(data => data['results'].slice(0, 6))
+      )
   }
   getUpcomingMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(`${BASE_URL}/upcoming${API_KEY}&page=1`)
-    .pipe(
-      map(data=>data['results'].slice(0, 6))
-    )
+      .pipe(
+        map(data => data['results'].slice(0, 6))
+      )
   }
   getTopRatedMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(`${BASE_URL}/top_rated${API_KEY}&page=1`)
-    .pipe(
-      map(data=>data['results'].slice(0, 6))
-    )
+      .pipe(
+        map(data => data['results'].slice(0, 6))
+      )
+  }
+  getMovieById(id: string){
+    return this.http.get<MovieDetails>(`${BASE_URL}/${id}${API_KEY}`)
   }
 }
