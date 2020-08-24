d044ea1bd58267a11bd0aedd32f1935344e33e01
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-08-24 20:52:35 +0300

    added functionality for searching movie by criteria

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -5,7 +5,7 @@ import { tap, map } from 'rxjs/operators'
 import Movie from '../models/Movie';
 import MovieDetails from '../models/Movie-Details'
 
-const BASE_URL = 'https://api.themoviedb.org/3/movie';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '?api_key=2802740079128ef16179086164af5f36';
 
 @Injectable({
@@ -18,30 +18,33 @@ export class MovieService {
   constructor(private http: HttpClient) { }
 
   getPopularMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${BASE_URL}/popular${API_KEY}&page=1`)
+    return this.http.get<Movie[]>(`${BASE_URL}movie/popular${API_KEY}&page=1`)
       .pipe(
         map(data => data['results'].slice(0, 6))
       )
   }
   getInTheaterMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${BASE_URL}/now_playing${API_KEY}&page=1`)
+    return this.http.get<Movie[]>(`${BASE_URL}movie/now_playing${API_KEY}&page=1`)
       .pipe(
         map(data => data['results'].slice(0, 6))
       )
   }
   getUpcomingMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${BASE_URL}/upcoming${API_KEY}&page=1`)
+    return this.http.get<Movie[]>(`${BASE_URL}movie/upcoming${API_KEY}&page=1`)
       .pipe(
         map(data => data['results'].slice(0, 6))
       )
   }
   getTopRatedMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${BASE_URL}/top_rated${API_KEY}&page=1`)
+    return this.http.get<Movie[]>(`${BASE_URL}movie/top_rated${API_KEY}&page=1`)
       .pipe(
         map(data => data['results'].slice(0, 6))
       )
   }
   getMovieById(id: string){
-    return this.http.get<MovieDetails>(`${BASE_URL}/${id}${API_KEY}`)
+    return this.http.get<MovieDetails>(`${BASE_URL}movie/${id}${API_KEY}`)
+  }
+  searchMovie(query: string){
+    return this.http.get<Movie[]>(`${BASE_URL}search/movie${API_KEY}&query=${query}`)
   }
 }
