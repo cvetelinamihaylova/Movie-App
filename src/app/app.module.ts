e3bd6e80f203b88f7f4f1a5f7a764a435b905654
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-08-24 20:53:40 +0300

    added FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { SingleMovieResolver } from './services/resolvers/single-movie.resolver';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { MovieComponent } from './movie/movie.component';
 import { FooterComponent } from './footer/footer.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
     MoviesComponent,
     MovieComponent,
     FooterComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    MovieSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [SingleMovieResolver],
